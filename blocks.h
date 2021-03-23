7ce364a8664910bbcae84f8ebb94ba0bd042b39a
9matt <matthvh@protonmail.ch>
2021-03-23 14:50:01 +0100

    moving scripts to dotfiles repo

--- blocks.h
+++ blocks.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/		/*Command*/	 		/*Update Interval*/	/*Update Signal*/
-  	{"", 		"/opt/dwmblocks/scripts/music",	  		60,		        4},
+  	{"", 		".local/bin/sb-scripts/music",	  		60,		        4},
 
-  	{" ", 		"/opt/dwmblocks/scripts/kernel",	  	0,		        5},
+  	{" ", 		".local/bin/sb-scripts/kernel",	  	0,		        5},
 
-	{" ", 		"/opt/dwmblocks/scripts/cputemp",	        2,		        6},
+	{" ", 		".local/bin/sb-scripts/cputemp",	        2,		        6},
 
-	{" ", 		"/opt/dwmblocks/scripts/memory",	        2,		        7},
+	{" ", 		".local/bin/sb-scripts/memory",	        2,		        7},
 
-	{" 🔊 ", 	"/opt/dwmblocks/scripts/volume",		2,		        10},
+	{" 🔊 ", 	".local/bin/sb-scripts/volume",		2,		        10},
 
-	{" ", 		"/opt/dwmblocks/scripts/kbdlayout",		0,		        20},
+	{" ", 		".local/bin/sb-scripts/kbdlayout",		0,		        20},
 	
-	{" ", 		"/opt/dwmblocks/scripts/clock",			5,		        30},
+	{" ", 		".local/bin/sb-scripts/clock",			5,		        30},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
